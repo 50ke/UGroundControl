a4d6c2b24ffac0ad8031020d01730cf3de4d31be
50ke <limelonrind@gmail.com>
2024-09-20 23:01:05 +0800

    开发无人船管理模块

--- src/VehicleManager/VehicleManager.cpp
+++ src/VehicleManager/VehicleManager.cpp
@@ -17,6 +17,26 @@ void UGC::VehicleManager::handleMessage(const mavlink_message_t &message){
         Vehicle vehicle{vehicleSystemId, name, longitude, latitude, UGC::VehicleType::survey};
         mVehicles.insert(vehicleSystemId, vehicle);
         emit vehiclesChanged(mVehicles.values());
+    }else if(message.msgid == MAVLINK_MSG_ID_USV_CONNECT_RESPONSE){
+        mavlink_usv_connect_response_t connect_response;
+        mavlink_msg_usv_connect_response_decode(&message, &connect_response);
+        int gcsSystemId = this->mApp->settingManager()->systemId();
+        if(connect_response.target_system == gcsSystemId && connect_response.ack == 1){
+            qDebug() << "Connect Vehicle Succeed: " << message.sysid;
+            emit connectVehicleCompleted(message.sysid);
+        }else{
+            qDebug() << "Connect Vehicle Failed: " << message.sysid;
+        }
+    }else if(message.msgid == MAVLINK_MSG_ID_USV_DISCONNECT_RESPONSE){
+        mavlink_usv_disconnect_response_t disconnect_response;
+        mavlink_msg_usv_disconnect_response_decode(&message, &disconnect_response);
+        int gcsSystemId = this->mApp->settingManager()->systemId();
+        if(disconnect_response.target_system == gcsSystemId && disconnect_response.ack == 1){
+            qDebug() << "Disconnect Vehicle Succeed: " << message.sysid;
+            emit disconnectVehicleCompleted(message.sysid);
+        }else{
+            qDebug() << "Disconnect Vehicle Failed: " << message.sysid;
+        }
     }
 }
 
